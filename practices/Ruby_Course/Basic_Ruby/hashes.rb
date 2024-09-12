b114ed4e06fe79b36c60e00993257488c6ab2a11
hope-miki <145734754+hope-miki@users.noreply.github.com>
2024-09-12 17:07:01 +0900

    Edit hashes practice text

--- practices/Ruby_Course/Basic_Ruby/hashes.rb
+++ practices/Ruby_Course/Basic_Ruby/hashes.rb
@@ -8,4 +8,15 @@ hashes = {    #One way of making hashes
 hash2 = Hash.new #Another way of making hashes
 hash2 = { 1 => "one", :five => 5 }
 
-p hash2[1]
+puts hash2[1]
+puts hash2.fetch(:five)
+
+hash2[1] = "five"
+puts hash2[1]
+
+hash2.delete(1)
+puts hash2
+
+hash2_key = hash2.keys
+hash2_value = hash2.values
+ 
